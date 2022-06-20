b16a743c45eec5d988d630674295bfb429e33ee8
bhoff <bruce.hoff@sagebase.org>
2022-06-20 08:55:15 -0700

    fixed text display

--- app.R
+++ app.R
@@ -12,9 +12,9 @@ server <- function(input, output, session) {
 
   output$currentTime <- renderText({
     invalidateLater(1000, session)
-    paste("The current time is", Sys.time())
-    paste("The value of env1 is: ", Sys.getenv("env1", unset="not set"))
-    paste("The value of env2 is: ", Sys.getenv("env2", unset="not set"))
+    paste("The current time is", Sys.time(),
+    "The value of env1 is:", Sys.getenv("env1", unset="not set"),
+    "The value of env2 is:", Sys.getenv("env2", unset="not set"))
   })
 
 }
